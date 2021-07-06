7d0ee60b453895336dfbac01939bc93964900100
Adam Miller <gaivs_petronivs@yahoo.com>
2021-07-05 19:50:36 -0500

    First Commit

---
+++ PetShopTest/Controllers/PetShopController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using PetShopTest.Models;
+
+namespace PetShopTest.Controllers
+{
+    public class PetShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            string baseurl = "https://petstore.swagger.io/";
+            List < Pets > PetList = new List<Pets>();
+
+            using (var client = new HttpClient())
+            {
+                var responseTask = client.GetAsync(baseurl + "v2/pet/findByStatus?status=available");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                var readTask = result.Content.ReadAsAsync<IList<Pets>>();
+                readTask.Wait();
+
+                foreach (var eachResult in readTask.Result)
+                {
+
+                    Pets newPet = new Pets();
+                    newPet.Id = eachResult.Id;
+                    newPet.Name = eachResult.Name;
+                    PetList.Add(newPet);
+                }
+
+            }
+
+            return View(PetList);
+        }
+    }
+}
---
+++ PetShopTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PetShopTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
